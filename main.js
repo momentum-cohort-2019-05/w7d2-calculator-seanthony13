efc3760c16763d1170b2cef0dbfbd46a7e89b4cc
seanthony13 <seanthony13@gmail.com>
2019-07-11 10:55:29 -0400

    button input shows in display

---
+++ main.js
@@ -0,0 +1,85 @@
+let display = document.querySelector('.display')
+
+document.querySelector('.clear').addEventListener('click', function (){
+    display.innerText = ""
+})
+
+let one = document.querySelector('.one')
+one.addEventListener('click', function (){
+    display.innerText += one.innerText
+})
+
+let two = document.querySelector('.two')
+two.addEventListener('click', function(){
+    display.innerText += two.innerText
+})
+
+let three = document.querySelector('.three')
+three.addEventListener('click', function(){
+    display.innerText += three.innerText
+})
+
+let four = document.querySelector('.four')
+four.addEventListener('click', function(){
+    display.innerText += four.innerText
+})
+
+let five = document.querySelector('.five')
+five.addEventListener('click', function(){
+    display.innerText += five.innerText
+})
+
+let six = document.querySelector('.six')
+six.addEventListener('click', function(){
+    display.innerText += six.innerText
+})
+
+let seven = document.querySelector('.seven')
+seven.addEventListener('click', function(){
+    display.innerText += seven.innerText
+})
+
+let eight = document.querySelector('.eight')
+eight.addEventListener('click', function(){
+    display.innerText += eight.innerText
+})
+
+let nine = document.querySelector('.nine')
+nine.addEventListener('click', function(){
+    display.innerText += nine.innerText
+})
+
+let zero = document.querySelector('.zero')
+zero.addEventListener('click', function(){
+    display.innerText += zero.innerText
+})
+
+let division = document.querySelector('.division')
+division.addEventListener('click', function(){
+    display.innerText += division.innerText
+})
+
+let multiplication = document.querySelector('.multiplication')
+multiplication.addEventListener('click', function(){
+    display.innerText += multiplication.innerText
+})
+
+let subtraction = document.querySelector('.subtraction')
+subtraction.addEventListener('click', function(){
+    display.innerText += subtraction.innerText
+})
+
+let addition = document.querySelector('.addition')
+addition.addEventListener('click', function(){
+    display.innerText += addition.innerText
+})
+
+let decimal = document.querySelector('.decimal')
+decimal.addEventListener('click', function(){
+    display.innerText += decimal.innerText
+})
+
+
+
+
+
